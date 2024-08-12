eafd96bc5654a484f55c147834fc1c9e50e405f2
Denise Grassi <denygrassi_@hotmail.com>
2024-08-12 10:36:27 -0300

    commit inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,28 @@
+AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2024 19:00:00"); /* atribuindo na constante dataDoEvento a data do evento futuro*/
+const timeStampDoEvento = dataDoEvento.getTime();  /* o getTime transforma a data do evento em milisegundos */
+
+const contaAsHoras = setInterval(function() {     /* Esse código continuará a executar a função a cada segundo (1000milisegundos), mostrando a contagem regressiva em dias até o evento.*/
+    const agora = new Date();   /* atribuindo na constante agora a data atual */
+    const timeStampAtual = agora.getTime(); /* o getTime transforma a data atual em milisegundos */
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;   /* faz o subtração da data do evento com a data atual em milissegundos*/
+
+    // Conversão de Milissegundos para Dias, Horas, Minutos e Segundos:
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+    
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);  /* math.floor para arredondar p baixo, garantir q eh inteiro */
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs); /* % é o resto da divisao da distancia do evento por horas em milisegundo */
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+}, 1000);
